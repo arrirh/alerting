484bc6fa983462304da14e0558aa8c89002d6e23
Alina Gerasimova <arrirh@gmail.com>
2022-02-09 14:40:14 +0000

    added improved alert script

--- alert_utils.py
+++ alert_utils.py
@@ -6,15 +6,49 @@ import seaborn as sns
 import pandahouse
 import telegram
 import io
+from statsmodels.tsa.seasonal import seasonal_decompose
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.ensemble import IsolationForest
 
-
-def is_outliers_IQR(data, time_col, metric):
-    q75, q25 = np.nanpercentile(data[metric], [75 ,25])
+def is_outliers_IQR(metric):
+    q75, q25 = np.nanpercentile(metric, [75 ,25])
     iqr = q75 - q25
     lower = q25 - 1.5*iqr
     upper = q75 + 1.5*iqr
-    is_outliers = np.logical_or(data[metric] > upper, data[metric] < lower)
-    return is_outliers.iloc[-1]
+    is_outliers = np.logical_or(metric > upper, metric < lower)
+    return is_outliers[len(is_outliers) - 1]
+
+def is_alert(query_for_prop, query_for_STL, y, connection):
+    #1
+    df = pandahouse.read_clickhouse(query_for_prop, connection=connection)
+
+    is_alert_prop = is_outliers_IQR(df[y])
+
+    #2
+    df = pandahouse.read_clickhouse(query_for_STL, connection=connection)
+
+    df = df[['minutes5', y]]
+    datetime_series = pd.to_datetime(df['minutes5'])
+    datetime_index = pd.DatetimeIndex(datetime_series.values)
+
+    df_for_STL = df.set_index(datetime_index)
+    df_for_STL.drop('minutes5', axis = 1, inplace = True)
+
+    result = seasonal_decompose(df_for_STL, period = 2, model='additive')
+
+    is_alert_resid = is_outliers_IQR(result.resid.values)
+
+    #3
+    scaler = StandardScaler()
+    np_scaled = scaler.fit_transform(df_for_STL.values.reshape(-1, 1))
+    data = pd.DataFrame(np_scaled)
+    model =  IsolationForest()
+    model.fit(data) 
+
+    is_alert_IF = (model.predict(data) == -1)[-1]
+
+    return int(is_alert_prop) + int(is_alert_resid) + int(is_alert_IF) >= 2
 
 
 def send_report(x, y, ylab, plot_title, plot_name, df, chat_id, bot,
